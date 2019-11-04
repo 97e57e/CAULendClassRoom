d8bcd0ca5ef01fbfd477b0260f51754216a56271
gardenLee <khjvvv7@gmail.com>
2019-11-05 00:33:40 +0900

    Discriminate Building's floors
    Building의 max_floor까지의 range를 이용하여
    건물의 층 구분 및 각 층 URL 매핑

--- main/views.py
+++ main/views.py
@@ -6,10 +6,10 @@ def index(request):
     buildings = Building.objects.all()
     return render(request, 'index.html', {'buildings' : buildings})
 
-def building(request, building_no):
+def building(request, building_no, floor):
     building = get_object_or_404(Building, pk=building_no)
-    rooms = ClassRoom.objects.filter(building_no=building_no)
-    return render(request, 'building.html', {'building' : building, 'rooms' : rooms})
+    rooms = ClassRoom.objects.filter(building_no=building_no, floor=floor)
+    return render(request, 'building.html', {'building' : building, 'rooms' : rooms, 'range' : range(1, building.max_floor+1)})
 
 def classroom(request, building_no, classroom_no):
     classroom = get_object_or_404(ClassRoom, building_no=building_no, room_no=classroom_no)
