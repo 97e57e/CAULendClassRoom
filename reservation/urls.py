c571e38eeb662c2a707cb859246edee20de84c06
gardenLee <khjvvv7@gmail.com>
2019-11-04 23:53:45 +0900

    Make Reservation Management Page
    예약 관리 페이지 생성 및 URL 매핑

--- reservation/urls.py
+++ reservation/urls.py
@@ -3,4 +3,5 @@ from reservation import views
 
 urlpatterns = [
     path('building/<int:building_no>/<int:classroom_no>/reservation', views.reservation, name='reservation'),
+    path('book-manage/', views.book_manage, name='book_manage'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def reservation(request, building_no, classroom_no):
     return render(request, 'reservation.html')
+
+def book_manage(request):
+    return render(request, 'book_manage.html')
