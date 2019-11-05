fa3e17aa1549605cc49c3ba8f642c5478d368c05
gardenLee <khjvvv7@gmail.com>
2019-11-05 16:54:50 +0900

    Make Book-management table

--- reservation/views.py
+++ reservation/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
+from .models import Reservation
 
 def reservation(request, building_no, classroom_no):
         return render(request, 'reservation.html')
 
 def book_manage(request):
-    return render(request, 'book_manage.html')
+    reservations = Reservation.objects.filter(user = request.user).order_by('-pk')
+    print(reservations)
+    return render(request, 'book_manage.html', {'reservations' : reservations})
