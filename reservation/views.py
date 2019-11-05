2ecc7f0ea3f2a33eae70e3341d5bdff1cf6dd6d6
gardenLee <khjvvv7@gmail.com>
2019-11-05 15:41:29 +0900

    Make Reservation form

--- reservation/views.py
+++ reservation/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def reservation(request, building_no, classroom_no):
-    return render(request, 'reservation.html')
+        return render(request, 'reservation.html')
 
 def book_manage(request):
     return render(request, 'book_manage.html')
