a7cc52cae1298f233e1330d012e79919b7d72b3c
gardenLee <khjvvv7@gmail.com>
2019-11-29 04:09:18 +0900

    검색 페이지 front 작업

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Building
 from .models import ClassRoom
 from reservation.reserve_valid_chk import is_reservation_valid
@@ -23,7 +24,7 @@ def search(request):
     print(request.POST.get('end_time'))
     print(request.POST.get('building'))
     building = get_object_or_404(Building, building_no = request.POST.get('building'))
-    rooms = building.classroom_set.all()
+    rooms = building.classroom_set.all().order_by('room_no')
     searched_room = []
     for room in rooms:
         if is_reservation_valid(room, request.POST.get('date'),request.POST.get('start_time'), request.POST.get('end_time')):
