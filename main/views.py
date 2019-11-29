279440c3c392fff0ee1222726da219feb8a36bfc
gardenLee <khjvvv7@gmail.com>
2019-11-29 20:31:09 +0900

    검색 셀렉트 박스 유지 시키기

--- main/views.py
+++ main/views.py
@@ -19,15 +19,13 @@ def classroom(request, building_no, classroom_no):
 
 def search(request):
     buildings = Building.objects.all()
-    print(request.POST.get('date'))
-    print(request.POST.get('start_time'))
-    print(request.POST.get('end_time'))
-    print(request.POST.get('building'))
+    date = request.POST.get('date')
+    start_time = request.POST.get('start_time')
+    end_time = request.POST.get('end_time')
     building = get_object_or_404(Building, building_no = request.POST.get('building'))
     rooms = building.classroom_set.all().order_by('room_no')
     searched_room = []
     for room in rooms:
         if is_reservation_valid(room, request.POST.get('date'),request.POST.get('start_time'), request.POST.get('end_time')):
             searched_room.append(room.room_no)
-    print(searched_room)
-    return render(request, 'search.html', {'buildings' : buildings, 'selected_building' : building, 'searched_room' : searched_room})
+    return render(request, 'search.html', {'date' : date, 'start_time' : start_time, 'end_time' : end_time, 'buildings' : buildings, 'selected_building' : building, 'searched_room' : searched_room})
